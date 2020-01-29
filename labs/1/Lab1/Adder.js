2094da233162ed8a7f71487e82cfe04f1c8bde65
Jo Folk <47342310+jo-folk@users.noreply.github.com>
2020-01-29 11:29:19 -0500

    lab1 commit

--- labs/1/Lab1/Adder.js
+++ labs/1/Lab1/Adder.js
@@ -1,4 +1,21 @@
-
 class Adder {
-    
+    constructor(a, b, _button){
+        this.button = _button;
+        this.props.a = a;
+        this.props.b = b;
+        this.props.sum;
+        //this.button.addEventListener("click", this.onClick)
+    }
+    sum() {
+        this.props.sum = this.props.a + this.props.b;
+        return (this.props.sum);
+    } 
+    onclick(){
+        this.sum();
+        this.render();
+    }
+    render(){
+        return '<p> The sum of '+this.props.a+' and '+this.props.b+' is '+this.props.sum+'</p>';
+    }
 }
+module.exports = Adder;
--- labs/1/Lab1/index.js
+++ labs/1/Lab1/index.js
@@ -1,1 +1,11 @@
+require('./Adder');
+
+
+let x = document.getElementById("a");
+let y = document.getElementById("b");
+
+let myAdder = new Adder(x, y);
+
+myAdder.onClick();
+
 
